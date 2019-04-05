995a6a368b808b7c69bcaa817579792e7ee84129
Vikram Karthikeyan <vkarthik@buffalo.edu>
2019-04-04 23:05:16 -0400

    Add data cleaning for 2017

---
+++ clean_2018.R
@@ -0,0 +1,80 @@
+## Installing required packagesyes
+install.packages("foreign", dependencies = TRUE) # for importing SPSS data
+install.packages("car", dependencies = TRUE) # for recoding functions
+install.packages("chron", dependencies = TRUE) # for changing time information
+install.packages("pastecs", dependencies = TRUE) # for basic descriptives
+install.packages("dplyr", dependencies = TRUE) # for basic descriptives
+
+## Loading required packages
+require(foreign)
+require(car)
+require(chron)
+require(pastecs)
+library("dplyr")
+
+clean_data <- function(year) {
+  file_name <- paste("/Users/vikramkarthikeyan/Documents/Kenny/IAT-Gender-Career-R/dataset/sav/", year, sep="")
+  file_name <- paste(file_name, ".sav", sep="")
+  
+  # Read IAT data
+  iat <- read.spss(file_name,
+                   to.data.frame = TRUE)
+  
+  ## to optimize, first reduce the number of variables...
+  iat <- iat[c("session_id", "session_status", "date", "year", "month", "day", "birthyear",
+                 "birthsex", "genderidentity","D_biep.Male_Career_all", "countrycit_num", 
+                 "countryres_num","ethnicityomb","raceomb_002")]
+
+  # convert date to ISO format
+  iat$date <- as.chron(ISOdate(1582, 10, 14) + iat$date) 
+  iat$date <- as.Date(iat$date)
+  
+  # Get only completed entries
+  iat <- iat[iat$session_status=="C   ",] 
+  
+  # First fuse ambiguous columns, god this is painful
+  if("sex" %in% colnames(iat) & "birthsex" %in% colnames(iat)) {
+    print("BOTH")
+    iat$birthsex <- factor(iat$birthsex, labels = c("Male", "Female"))
+    iat$sex <- with(iat, coalesce(sex, factor(birthsex)))
+  } else {
+    iat$sex <- iat$birthsex
+  }
+  
+  iat <- iat[is.na(iat$sex)==FALSE,,]
+  
+  # Remove those entries in which important fields are not present - Race, Ethnicity, Gender
+  iat <- iat[is.na(iat$ethnicityomb)==FALSE,]
+  iat <- iat[is.na(iat$raceomb_002)==FALSE,]
+  iat <- iat[is.na(iat$countryres_num)==FALSE,]
+  iat <- iat[is.na(iat$countrycit_num)==FALSE,]
+  iat <- iat[is.na(iat$birthyear)==FALSE,]
+  
+  # Get those entries only from U.S.A
+  iat$countrycit <- factor(iat$countrycit_num)
+  iat$countrycit <- gsub(" ", "", iat$countrycit)
+  
+  iat$countryres <- factor(iat$countryres_num)
+  iat$countryres <- gsub(" ", "", iat$countryres)
+  iat <- iat[iat$countrycit=="U.S.A.",]
+  iat <- iat[iat$countryres=="U.S.A.",]
+  
+  ## change birthyear to age------------
+
+  table(iat$birthyear, iat$year) # this one picks up...
+  table(iat$birthyear) # only uses real year values...
+  
+  iat$age <- year - iat$birthyear
+  
+  return(iat)
+}
+
+
+result <- clean_data(2017)
+
+write.csv(result, file = "/Users/vikramkarthikeyan/Documents/Kenny/IAT-Gender-Career-R/dataset/2018.csv")
+
+
+
+
+
