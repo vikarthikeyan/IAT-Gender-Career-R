bf271e8fbb42f59b7386f81b8d0d29ff88e11580
EmmanuelJohnson <emmanuel16johnson@gmail.com>
2019-05-10 00:35:37 -0400

    Extracted additive values

--- experimentation.R
+++ experimentation.R
@@ -2,7 +2,9 @@
 install.packages("fitdistrplus", dependencies = TRUE) 
 install.packages("logspline", dependencies = TRUE) 
 install.packages("mgcv", dependencies = TRUE) 
-install.packages("nlme", dependencies = TRUE) 
+install.packages("nlme", dependencies = TRUE)
+install.packages("remotes")
+remotes::install_github("mfasiolo/mgcViz", dependencies = TRUE)
 
 ## Loading required packages
 library(fitdistrplus)
@@ -10,9 +12,10 @@ library(logspline)
 library(mgcv)
 library(nlme)
 library(visreg)
+library(mgcViz)
 
 
-data = read.csv("/Users/vikramkarthikeyan/Documents/Kenny/IAT-Gender-Career-R/dataset/cleaned-2007-2017.csv", header = TRUE)
+data = read.csv("/Users/bobbyda/IAT-Gender-Career-R/dataset/cleaned-2007-2017.csv", header = TRUE)
 
 data$date <- as.numeric(data$date)
 
@@ -33,12 +36,25 @@ plot(model_time_age_sex_month, scale = 0, shade=TRUE)
 
 model_time_age_sex_month_state <- gam(D_biep.Male_Career_all ~ s(date, by=STATE) + s(age) + s(month, bs = "cc", k = 12) + sex, select=TRUE, method='GCV.Cp', data = data)
 
-#par(mfrow= c(1,1))
+
+pp <- predict(model_time_age_sex_month_state, type = "terms")
+write.csv(pp, file = "additive_values.csv")
+
+
+
+
+#par(mfrow= c(5,2))
 #plot(model_time_age_sex_month_state, scale = 0, shade=TRUE)
-#plot(model_time_age_sex_month_state, shade=TRUE, select=3)
+
+#model <- getViz(model_time_age_sex_month_state)
+#mgcv::plot.gam(model)
+
+#plot( sm(model, 1), type = "l", col="red" )
+#points( sm(model, 2), type="l", col="green",add=TRUE )
+#print(plot(model), ask = FALSE)
 
 #visreg(model_time_age_sex_month_state)
-visreg(model_time_age_sex_month_state, "date", "STATE", gg=TRUE, collapse=TRUE, overlay=TRUE, ylab="IAT Score")
+#visreg(model_time_age_sex_month_state, "date", "STATE", gg=TRUE, collapse=TRUE, overlay=TRUE, ylab="IAT Score")
 
 
 
